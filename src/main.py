6ebd81902b39726d8e820f4f7b48fd5190d49600
Moustafa Awny Khairi <mostafa.awny91@gmail.com>
2024-07-31 21:42:27 +0300

    clean code

--- src/main.py
+++ src/main.py
@@ -6,8 +6,8 @@ from joblib import load
 
 import pandas as pd
 import numpy as np
-#from model import train_model, compute_model_metrics, inference
-#from data import clean_data
+from model import train_model, compute_model_metrics, inference
+from data import process_data
 import os
 
 
@@ -19,144 +19,10 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import LabelBinarizer, OneHotEncoder
 
 
-def process_data(
-    X, categorical_features=[], label=None, training=True, encoder=None, lb=None
-):
-    """ Process the data used in the machine learning pipeline.
-
-    Processes the data using one hot encoding for the categorical features and a
-    label binarizer for the labels. This can be used in either training or
-    inference/validation.
-
-    Note: depending on the type of model used, you may want to add in functionality that
-    scales the continuous data.
-cd 
-    Inputs
-    ------
-    X : pd.DataFrame
-        Dataframe containing the features and label. Columns in `categorical_features`
-    categorical_features: list[str]
-        List containing the names of the categorical features (default=[])
-    label : str
-        Name of the label column in `X`. If None, then an empty array will be returned
-        for y (default=None)
-    training : bool
-        Indicator if training mode or inference/validation mode.
-    encoder : sklearn.preprocessing._encoders.OneHotEncoder
-        Trained sklearn OneHotEncoder, only used if training=False.
-    lb : sklearn.preprocessing._label.LabelBinarizer
-        Trained sklearn LabelBinarizer, only used if training=False.
-
-    Returns
-    -------
-    X : np.array
-        Processed data.
-    y : np.array
-        Processed labels if labeled=True, otherwise empty np.array.
-    encoder : sklearn.preprocessing._encoders.OneHotEncoder
-        Trained OneHotEncoder if training is True, otherwise returns the encoder passed
-        in.
-    lb : sklearn.preprocessing._label.LabelBinarizer
-        Trained LabelBinarizer if training is True, otherwise returns the binarizer
-        passed in.
-    """
-
-    if label is not None:
-        y = X[label]
-        X = X.drop([label], axis=1)
-    else:
-        y = np.array([])
-
-    X_categorical = X[categorical_features].values
-    X_continuous = X.drop(*[categorical_features], axis=1)
-
-    if training is True:
-        encoder = OneHotEncoder(sparse=False, handle_unknown="ignore")
-        lb = LabelBinarizer()
-        X_categorical = encoder.fit_transform(X_categorical)
-        y = lb.fit_transform(y.values).ravel()
-    else:
-        X_categorical = encoder.transform(X_categorical)
-        try:
-            y = lb.transform(y.values).ravel()
-        # Catch the case where y is None because we're doing inference.
-        except AttributeError:
-            pass
-
-    X = np.concatenate([X_continuous, X_categorical], axis=1)
-    print(X.dtype)
-    return X, y, encoder, lb
-
-# Optional: implement hyperparameter tuning.
-def train_model(X_train, y_train):
-    """
-    Trains a machine learning model and returns it.
-
-    Inputs
-    ------
-    X_train : np.array
-        Training data.
-    y_train : np.array
-        Labels.
-    Returns
-    -------
-    model
-        Trained machine learning model.
-    """
-    try:
-        model = RandomForestClassifier()
-        model.fit(X_train, y_train)
-        logging.info('Model trained successfully')
-        return model
-    except BaseException:
-        logging.info('Model training or saving failed')
 
 
-def compute_model_metrics(y, preds):
-    """
-    Validates the trained machine learning model using precision, recall, and F1.
-
-    Inputs
-    ------
-    y : np.array
-        Known labels, binarized.
-    preds : np.array
-        Predicted labels, binarized.
-    Returns
-    -------
-    precision : float
-    recall : float
-    fbeta : float
-    """
-    fbeta = fbeta_score(y, preds, beta=1, zero_division=1)
-    precision = precision_score(y, preds, zero_division=1)
-    recall = recall_score(y, preds, zero_division=1)
-    return precision, recall, fbeta
 
 
-def inference(model, X):
-    """ Run model inferences and return the predictions.
-
-    Inputs
-    ------
-    model : ???
-        Trained machine learning model.
-    X : np.array
-        Data used for prediction.
-    Returns
-    -------
-    preds : np.array
-        Predictions from the model.
-    """
-    preds = model.predict(X)
-    return preds
-
-
-if "DYNO" in os.environ and os.path.isdir(".dvc"):
-    os.system("dvc config core.no_scm true")
-    if os.system("dvc pull") != 0:
-        exit("dvc pull failed")
-    os.system("rm -r .dvc .apt/usr/lib/dvc")
 
 cat_features = [
     "workclass",
@@ -173,30 +39,21 @@ cat_features = [
 class User(BaseModel):
     age: int
     workclass: Literal[
-        'State-gov', 'Self-emp-not-inc', 'Private', 'Federal-gov',
-        'Local-gov', 'Self-emp-inc', 'Without-pay']
+        'State-gov', 'Self-emp-not-inc', 'Private', 'Federal-gov', 'Local-gov', 'Self-emp-inc', 'Without-pay']
     education: Literal[
-        'Bachelors', 'HS-grad', '11th', 'Masters', '9th',
-        'Some-college',
-        'Assoc-acdm', '7th-8th', 'Doctorate', 'Assoc-voc', 'Prof-school',
-        '5th-6th', '10th', 'Preschool', '12th', '1st-4th']
+        'Bachelors', 'HS-grad', '11th', 'Masters', '9th', 'Some-college', 'Assoc-acdm', '7th-8th', 'Doctorate', 
+        'Assoc-voc', 'Prof-school', '5th-6th', '10th', 'Preschool', '12th', '1st-4th']
     maritalStatus: Literal[
-        'Never-married', 'Married-civ-spouse', 'Divorced',
-        'Married-spouse-absent', 'Separated', 'Married-AF-spouse',
-        'Widowed']
+        'Never-married', 'Married-civ-spouse', 'Divorced', 'Married-spouse-absent', 'Separated', 'Married-AF-spouse', 'Widowed']
     occupation: Literal[
-        'Adm-clerical', 'Exec-managerial', 'Handlers-cleaners',
-        'Prof-specialty', 'Other-service', 'Sales', 'Transport-moving',
-        'Farming-fishing', 'Machine-op-inspct', 'Tech-support',
-        'Craft-repair', 'Protective-serv', 'Armed-Forces',
-        'Priv-house-serv']
+        'Adm-clerical', 'Exec-managerial', 'Handlers-cleaners', 'Prof-specialty', 'Other-service', 'Sales', 'Transport-moving',
+        'Farming-fishing', 'Machine-op-inspct', 'Tech-support', 'Craft-repair', 'Protective-serv', 'Armed-Forces', 'Priv-house-serv']
     relationship: Literal[
-        'Not-in-family', 'Husband', 'Wife', 'Own-child',
-        'Unmarried', 'Other-relative']
+        'Not-in-family', 'Husband', 'Wife', 'Own-child', 'Unmarried', 'Other-relative']
     race: Literal[
-        'White', 'Black', 'Asian-Pac-Islander', 'Amer-Indian-Eskimo',
-        'Other']
-    sex: Literal['Male', 'Female']
+        'White', 'Black', 'Asian-Pac-Islander', 'Amer-Indian-Eskimo', 'Other']
+    sex: Literal[
+        'Male', 'Female']
     hoursPerWeek: int
     nativeCountry: Literal[
         'United-States', 'Cuba', 'Jamaica', 'India', 'Mexico',
@@ -237,22 +94,10 @@ async def inferences(user_data: User):
                      user_data.nativeCountry
                      ]])
 
-    df = pd.DataFrame(data=array, columns=[
-        "age",
-        "workclass",
-        "education",
-        "marital-status",
-        "occupation",
-        "relationship",
-        "race",
-        "sex",
-        "hours-per-week",
-        "native-country",
-    ])
-    X, _, _, _ = process_data(
-        df,
-        categorical_features=cat_features,
-        encoder=encoder, lb=lb, training=False)
+    df = pd.DataFrame(data=array, columns=["age", "workclass", "education", "marital-status", "occupation", "relationship", "race", "sex",
+                      "hours-per-week", "native-country"])
+                      
+    X, y, e, l = process_data(df, categorical_features=cat_features, encoder=encoder, lb=lb, training=False)
     pred = inference(model, X)
     y = lb.inverse_transform(pred)[0]
     return {"prediction": y}
--- src/test_api.py
+++ src/test_api.py
@@ -1,254 +1,8 @@
 import pytest
+import pandas as pd
+import numpy as np
 from fastapi.testclient import TestClient
-import pandas as pd
-import numpy as np
-from sklearn.preprocessing import LabelBinarizer, OneHotEncoder
-
-# Put the code for your API here.
-from typing import Literal
-from fastapi import FastAPI
-from pydantic import BaseModel
-from joblib import load
-
-import pandas as pd
-import numpy as np
-#from model import train_model, compute_model_metrics, inference
-#from data import clean_data
-import os
-
-
-from sklearn.metrics import fbeta_score, precision_score, recall_score
-import logging
-from sklearn.ensemble import RandomForestClassifier
-
-def train_model(X_train, y_train):
-    """
-    Trains a machine learning model and returns it.
-
-    Inputs
-    ------
-    X_train : np.array
-        Training data.
-    y_train : np.array
-        Labels.
-    Returns
-    -------
-    model
-        Trained machine learning model.
-    """
-    try:
-        model = RandomForestClassifier()
-        model.fit(X_train, y_train)
-        logging.info('Model trained successfully')
-        return model
-    except BaseException:
-        logging.info('Model training or saving failed')
-
-
-def compute_model_metrics(y, preds):
-    """
-    Validates the trained machine learning model using precision, recall, and F1.
-
-    Inputs
-    ------
-    y : np.array
-        Known labels, binarized.
-    preds : np.array
-        Predicted labels, binarized.
-    Returns
-    -------
-    precision : float
-    recall : float
-    fbeta : float
-    """
-    fbeta = fbeta_score(y, preds, beta=1, zero_division=1)
-    precision = precision_score(y, preds, zero_division=1)
-    recall = recall_score(y, preds, zero_division=1)
-    return precision, recall, fbeta
-
-
-def inference(model, X):
-    """ Run model inferences and return the predictions.
-
-    Inputs
-    ------
-    model : ???
-        Trained machine learning model.
-    X : np.array
-        Data used for prediction.
-    Returns
-    -------
-    preds : np.array
-        Predictions from the model.
-    """
-    preds = model.predict(X)
-    return preds
-
-
-cat_features = [
-    "workclass",
-    "education",
-    "marital-status",
-    "occupation",
-    "relationship",
-    "race",
-    "sex",
-    "native-country",
-    ]
-def process_data(
-    X, categorical_features=[], label=None, training=True, encoder=None, lb=None
-):
-    """ Process the data used in the machine learning pipeline.
-
-    Processes the data using one hot encoding for the categorical features and a
-    label binarizer for the labels. This can be used in either training or
-    inference/validation.
-
-    Note: depending on the type of model used, you may want to add in functionality that
-    scales the continuous data.
-cd 
-    Inputs
-    ------
-    X : pd.DataFrame
-        Dataframe containing the features and label. Columns in `categorical_features`
-    categorical_features: list[str]
-        List containing the names of the categorical features (default=[])
-    label : str
-        Name of the label column in `X`. If None, then an empty array will be returned
-        for y (default=None)
-    training : bool
-        Indicator if training mode or inference/validation mode.
-    encoder : sklearn.preprocessing._encoders.OneHotEncoder
-        Trained sklearn OneHotEncoder, only used if training=False.
-    lb : sklearn.preprocessing._label.LabelBinarizer
-        Trained sklearn LabelBinarizer, only used if training=False.
-
-    Returns
-    -------
-    X : np.array
-        Processed data.
-    y : np.array
-        Processed labels if labeled=True, otherwise empty np.array.
-    encoder : sklearn.preprocessing._encoders.OneHotEncoder
-        Trained OneHotEncoder if training is True, otherwise returns the encoder passed
-        in.
-    lb : sklearn.preprocessing._label.LabelBinarizer
-        Trained LabelBinarizer if training is True, otherwise returns the binarizer
-        passed in.
-    """
-
-    if label is not None:
-        y = X[label]
-        X = X.drop([label], axis=1)
-    else:
-        y = np.array([])
-
-    X_categorical = X[categorical_features].values
-    X_continuous = X.drop(*[categorical_features], axis=1)
-
-    if training is True:
-        encoder = OneHotEncoder(sparse=False, handle_unknown="ignore")
-        lb = LabelBinarizer()
-        X_categorical = encoder.fit_transform(X_categorical)
-        y = lb.fit_transform(y.values).ravel()
-    else:
-        X_categorical = encoder.transform(X_categorical)
-        try:
-            y = lb.transform(y.values).ravel()
-        # Catch the case where y is None because we're doing inference.
-        except AttributeError:
-            pass
-
-    X = np.concatenate([X_continuous, X_categorical], axis=1)
-    print(X.dtype)
-    return X, y, encoder, lb
-#from main import app
-class User(BaseModel):
-    age: int
-    workclass: Literal[
-        'State-gov', 'Self-emp-not-inc', 'Private', 'Federal-gov',
-        'Local-gov', 'Self-emp-inc', 'Without-pay']
-    education: Literal[
-        'Bachelors', 'HS-grad', '11th', 'Masters', '9th',
-        'Some-college',
-        'Assoc-acdm', '7th-8th', 'Doctorate', 'Assoc-voc', 'Prof-school',
-        '5th-6th', '10th', 'Preschool', '12th', '1st-4th']
-    maritalStatus: Literal[
-        'Never-married', 'Married-civ-spouse', 'Divorced',
-        'Married-spouse-absent', 'Separated', 'Married-AF-spouse',
-        'Widowed']
-    occupation: Literal[
-        'Adm-clerical', 'Exec-managerial', 'Handlers-cleaners',
-        'Prof-specialty', 'Other-service', 'Sales', 'Transport-moving',
-        'Farming-fishing', 'Machine-op-inspct', 'Tech-support',
-        'Craft-repair', 'Protective-serv', 'Armed-Forces',
-        'Priv-house-serv']
-    relationship: Literal[
-        'Not-in-family', 'Husband', 'Wife', 'Own-child',
-        'Unmarried', 'Other-relative']
-    race: Literal[
-        'White', 'Black', 'Asian-Pac-Islander', 'Amer-Indian-Eskimo',
-        'Other']
-    sex: Literal['Male', 'Female']
-    hoursPerWeek: int
-    nativeCountry: Literal[
-        'United-States', 'Cuba', 'Jamaica', 'India', 'Mexico',
-        'Puerto-Rico', 'Honduras', 'England', 'Canada', 'Germany', 'Iran',
-        'Philippines', 'Poland', 'Columbia', 'Cambodia', 'Thailand',
-        'Ecuador', 'Laos', 'Taiwan', 'Haiti', 'Portugal',
-        'Dominican-Republic', 'El-Salvador', 'France', 'Guatemala',
-        'Italy', 'China', 'South', 'Japan', 'Yugoslavia', 'Peru',
-        'Outlying-US(Guam-USVI-etc)', 'Scotland', 'Trinadad&Tobago',
-        'Greece', 'Nicaragua', 'Vietnam', 'Hong', 'Ireland', 'Hungary',
-        'Holand-Netherlands']
-
-
-app = FastAPI()
-
-
-@app.get("/")
-async def get_items():
-    return {"message": "Welcome!"}
-
-
-@app.post("/")
-async def inferences(user_data: User):
-    #model = load("model.joblib")
-    #encoder = load("encoder.joblib")
-    #lb = load("lb.joblib")
-
-    array = np.array([[
-                     user_data.age,
-                     user_data.workclass,
-                     user_data.education,
-                     user_data.maritalStatus,
-                     user_data.occupation,
-                     user_data.relationship,
-                     user_data.race,
-                     user_data.sex,
-                     user_data.hoursPerWeek,
-                     user_data.nativeCountry
-                     ]])
-
-    df = pd.DataFrame(data=array, columns=[
-        "age",
-        "workclass",
-        "education",
-        "marital-status",
-        "occupation",
-        "relationship",
-        "race",
-        "sex",
-        "hours-per-week",
-        "native-country",
-    ])
-    #X, _, _, _ = process_data(
-    #    df,
-    #    categorical_features=cat_features,
-    #   encoder=encoder, lb=lb, training=False)
-    #pred = inference(model, X)
-    #y = lb.inverse_transform(pred)[0]
-    #return {"prediction": y}
+from main import app
 
 
 @pytest.fixture
@@ -268,25 +22,25 @@ def test_get(client):
 
 
 def test_get_malformed(client):
-    r = client.get("/wrong_url")
+    r = client.get("/oops")
     assert r.status_code != 200
 
 
 def test_post_above(client):
     r = client.post("/", json={
-        "age": 60,
-        "workclass": "Private",
-        "education": "Doctorate",
-        "maritalStatus": "Divorced",
-        "occupation": "Transport-moving",
-        "relationship": "Not-in-family",
+        "age": 39,
+        "workclass": " State-gov",
+        "education": " Bachelors",
+        "maritalStatus": " Never-married",
+        "occupation": " Adm-clerical",
+        "relationship": " Not-in-family",
         "race": "White",
         "sex": "Male",
-        "hoursPerWeek": 76,
+        "hoursPerWeek": 40,
         "nativeCountry": "United-States"
     })
     assert r.status_code == 200
-    #assert r.json() == {"prediction": "<=50K"}
+    assert r.json() == {"prediction": "<=50K"}
 
 
 def test_post_below(client):
@@ -303,20 +57,20 @@ def test_post_below(client):
         "nativeCountry": "United-States"
     })
     assert r.status_code == 200
-    #sassert r.json() == {"prediction": "<=50K"}
+    assert r.json() == {"prediction": "<=50K"}
 
 
 def test_post_malformed(client):
     r = client.post("/", json={
-        "age": 32,
+        "age": 28,
         "workclass": "Private",
-        "education": "Some-college",
-        "maritalStatus": "ERROR",
-        "occupation": "Exec-managerial",
-        "relationship": "Husband",
+        "education": " Bachelors",
+        "maritalStatus": " Married-civ-spouse",
+        "occupation": " Prof-specialty",
+        "relationship": " Wife",
         "race": "Black",
-        "sex": "Male",
-        "hoursPerWeek": 60,
-        "nativeCountry": "United-States"
+        "sex": "Female",
+        "hoursPerWeek": 40,
+        "nativeCountry": "Cuba"
     })
     assert r.status_code != 200
